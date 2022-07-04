1a95e0de2f19d5bb660f58c7cfeb2f35d0768fee
Brent Wilske <wilskeworkflow@gmail.com>
2022-07-04 15:48:51 -0700

    minor ui fixes and such, done for now

--- index.js
+++ index.js
@@ -163,6 +163,7 @@ const helperFunctions = (() => {
       console.log(clearButtonEl);
       clearButtonEl.addEventListener("click", gameBoard.clearBoard, false);
     },
+    getPlayerNames: () => {},
   };
 })();
 
