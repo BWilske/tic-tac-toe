be2ca1960d3813ed2d24123e7ab2f1a9815dfee8
Brent Wilske <wilskeworkflow@gmail.com>
2022-07-04 08:34:00 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const gameBoard = (() => {
+  return { num: 1 };
+})();
+
+const player = (title, playerNum) => {
+  return {
+    title,
+    playerNum,
+  };
+};
+
+const gameState = () => {
+  return {};
+};
